408e914c5fe76075e8cf32b47ed074233855cd22
kirkrut <43215374+kirkrut@users.noreply.github.com>
2018-09-26 18:16:45 +0300

    готово

--- task-datarecovery2.py
+++ task-datarecovery2.py
@@ -1,23 +1,18 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-import random
-
+import scipy.stats
+import numpy as np
 scope= ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 creds= ServiceAccountCredentials.from_json_keyfile_name('secret_file.json', scope)
 client= gspread.authorize(creds)
 
 wks= client.open("Numbers").sheet1
-wks.clear()
+
 n=10
+list = []
 for i in range(1,n+1):
     for p in range(1,n+1):
-        wks.update_cell(i,p,int(random.uniform(1,30)))
-i=0
-while i<10:
-    x = round(random.uniform(1, 10))
-    y = round(random.uniform(1, 10))
-    if int(wks.cell(x,y).value)!=0:
-        wks.update_cell(x,y,0)
-        i += 1
-    if int(wks.cell(x, y).value)== 0:
-        continue
+        list.append(int(wks.cell(i, p).value))
+
+print(scipy.stats.mstats.winsorize(list,limits=0.05))
+
