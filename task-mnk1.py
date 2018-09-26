550d7f716d2ac49e92d122fd7bf72d014bc14cef
kirkrut <43215374+kirkrut@users.noreply.github.com>
2018-09-26 15:32:01 +0300

    готово

--- task-mnk1.py
+++ task-mnk1.py
@@ -6,10 +6,14 @@ scope= ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/aut
 creds= ServiceAccountCredentials.from_json_keyfile_name('secret_file.json', scope)
 client= gspread.authorize(creds)
 
+n = 10
+
 wks= client.open("Numbers").sheet1
 ans = [int(i) for i in input("Введите диапазон значений через пробел\n").split()]
-for i in range(0,10):
-    wks.append_row([random.uniform(ans[0],ans[1]),random.uniform(ans[0],ans[1])])
+wks.clear()
+wks.append_row(['x','y'])
+for i in range(0,n):
+    wks.append_row([int(random.uniform(ans[0],ans[1])),int(random.uniform(ans[0],ans[1]))])
 
 #num= wks.get_all_records()
 #print(num)
